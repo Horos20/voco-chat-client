b8353df468b1f60d07d9beb8eb988f573515f71a
Matis Rammi <matis.rammi@khk.ee>
2022-02-02 22:35:17 +0200

    Styling with css

--- src/chat.js
+++ src/chat.js
@@ -1,40 +1,41 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+const DUMMY_DATA = [
+    {
+        senderId: 'John Smith',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+    },
+    {
+        senderId: 'John Smith',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    }
+]
 
 function Chat() {
     return (
-        <div className="App">
-            <div className="page-container">
-                <h1>VOCO</h1>
-                <div className="chat-box">
+        <Container className="page-container">
+            <h1>VOCO</h1>
+            <div className="chat-container">
                     <div className="messages-box">
-                        <div className="text-box">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="text-box">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="text-box">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="text-box">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="text-box">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
+                        {DUMMY_DATA.map((message, index) => {
+                            return (
+                                <div className="text-box" key={index}>
+                                    <div className="message" >{message.text}</div>
+                                    <div className="user-name" >{message.senderId}</div>
+                                </div>
+                            )
+                        })}
                     </div>
                     <div className="user-box">
-                        <div className="user">
-                            <p>Username</p>
-                        </div>
-                        <div className="user-message">
-                            <p>Lore psum dolor sit amet.</p>
-                        </div>
+                        <div className="user"><b>John Smith</b></div>
+                        <div className="user-message">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</div>
                     </div>
-                </div>
-                <button>Send</button>
             </div>
-        </div>
+            <Button className="btn-block">Send</Button>
+        </Container>
     );
 }
 
