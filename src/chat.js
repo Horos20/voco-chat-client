96f03582cfabf8a687f5dc2e7c49cebf6ff631d1
Matis Rammi <matis.rammi@khk.ee>
2022-02-13 23:56:02 +0200

    Add message functionality

--- src/chat.js
+++ src/chat.js
@@ -2,39 +2,54 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-
-const DUMMY_DATA = [
-    {
-        senderId: 'John Smith',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-    },
-    {
-        senderId: 'John Smith',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
-    }
-]
+import {useState} from "react";
 
 function Chat() {
+
+    const DUMMY_DATA = [
+        {
+            userName: 'John Smith',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+        },
+        {
+            userName: 'John Smith',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+        }
+    ]
+
+    const [messages, newMessages] = useState(DUMMY_DATA);
+    const [text, setText] = useState('');
+    const userName = 'John Smith';
+
+    function textChange(event) {
+        setText(event.target.value);
+    }
+
+    function addNewMessage() {
+        const newMessage = messages.concat({ userName, text });
+        newMessages(newMessage);
+    }
+
     return (
         <Container className="page-container">
             <h1>VOCO</h1>
             <div className="chat-container">
                     <div className="messages-box">
-                        {DUMMY_DATA.map((message, index) => {
+                        {messages.map((message, index) => {
                             return (
-                                <div className="text-box" key={index}>
+                                <div className="text-box" id="text-box" key={index}>
                                     <div className="message" >{message.text}</div>
-                                    <div className="user-name" >{message.senderId}</div>
+                                    <div className="user-name" >{message.userName}</div>
                                 </div>
                             )
                         })}
                     </div>
                     <div className="user-box">
-                        <div className="user"><b>John Smith</b></div>
-                        <div className="user-message">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</div>
+                        <div className="user" id="user">John Smith</div>
+                        <div className="user-message"><textarea id="message" onChange={textChange}></textarea></div>
                     </div>
             </div>
-            <Button className="btn-block">Send</Button>
+            <Button className="btn-block" onClick={addNewMessage}>Send</Button>
         </Container>
     );
 }
