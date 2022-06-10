38f5c06a091711b5699bf01241451e4b9bce7b80
Matis Rammi <matisrammi@gmail.com>
2022-06-10 22:50:49 +0300

    Added socketio

--- src/chat.js
+++ src/chat.js
@@ -2,43 +2,56 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import {useState, useEffect} from "react";
+import io from "socket.io-client";
 
 function Chat() {
+    const socket = io('http://localhost:8080');
 
     const [messages, newMessages] = useState([]);
     const [text, setText] = useState('');
     const [userName, setUserName] = useState('John Smith');
 
+    socket.on('receive-message', (message, user) => {
+        newMessages([...messages, {text: message, userName: user}])
+    })
+
     useEffect(() => {
+        /*   Socket.io connection   */
+        socket.on("connect", () => {
+            console.log("You are connected")
+        })
+
         const getData = () => {
           return fetch('http://localhost:8080', {method: 'GET'}
-          ).then (res => {
+        ).then (res => {
             return res.json()
-          }).then (data => {
+        }).then (data => {
             newMessages(data)
-          })
+        })
         }
         getData();
-      }, []);
+    }, []);
 
     function addNewMessage(e) {
         e.preventDefault();
 
+        
         if (text === '') {
             return "Input empty"
         } else {
             fetch('http://localhost:8080', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-            userName: userName,
-            text: text
-            })
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    userName: userName,
+                    text: text
+                })
             }).then(res => {
-            return res.json()
+                return res.json()
             }).then(data => newMessages([...messages, data]))
+            socket.emit('sendMessage', text, userName)
         }
         
         setText('')
